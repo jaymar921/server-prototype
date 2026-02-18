dc077794f4bc3a01c85c0dd469e5b2c28c287a0b
JayMar921 <jayharronabejar@gmail.com>
2026-02-18 23:08:21 +0800

    Added more security flaw

--- src/routes/ipwpDeviceLocateRouter.js
+++ src/routes/ipwpDeviceLocateRouter.js
@@ -13,8 +13,8 @@ import {
 
 const router = Router();
 
-router.get("/", authenticateUser, getIPWP_LocateDevices);
-router.get("/get_location", authenticateOperator, getIPWP_LocateDevice);
+router.get("/", getIPWP_LocateDevices);
+router.get("/get_location", getIPWP_LocateDevice);
 router.post("/register", authenticateOperator, createIPWP_TR_LOCATE_Device);
 router.post("/update", updateIPWP_LocateDevice); // TODO: Secure this!!! This is not secure but will work on it soon
 router.delete("/", authenticateOperator, deleteIPWP_LocateDevice);
